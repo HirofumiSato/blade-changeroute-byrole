523646e14f33ccc8e1151059b9bd4618114d3858
HirofumiSato <midnight.8718@gmail.com>
2024-04-23 18:22:58 +0900

    UserFactoryの修正

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        //User::factory(50)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Asmin User',
+            'is_admin' => true,
+            'email' => 'admin@example.com',
+        ]);
+    }
+}
